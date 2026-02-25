c78ee93aad4b64e055ba7f5b1ecd04ffcd8ebd97
Sandro Gakharia <sandrogakharia@gmail.com>
2026-02-25 20:54:07 +0400

    Fix port conflicts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,3 +5,12 @@ import dotenv from 'dotenv';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("API is running");
+});
+
+
+app.listen(5050, () => {
+    console.log("Server is running on port 5050");
+})
