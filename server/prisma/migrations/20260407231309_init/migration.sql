7550f488ec1d264c8eb9b943f55d9ba859a4d022
Sandro Gakharia <sandrogakharia@gmail.com>
2026-04-08 03:18:00 +0400

    update .gitignore

---
+++ server/prisma/migrations/20260407231309_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('TO_DO', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'TO_DO',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
